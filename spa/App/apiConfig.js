ab43ddd44f8fee3dd042aaa9963966afce1eee11
Adamo Buosi <adamo.buosi@gmail.com>
2021-01-05 18:33:52 -0300

    First upload

---
+++ spa/App/apiConfig.js
@@ -0,0 +1,5 @@
+// Add here the endpoints and scopes for the web API you would like to use.
+const apiConfig = {
+    uri: "http://localhost:5000/api", 
+    scopes: ["<api scope>"] // get value on AzureAD
+};
